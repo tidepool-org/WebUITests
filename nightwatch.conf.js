0d55b6e3e4ee200135998be365e48aac0887b40f
Ginny Yadav <ginny@tidepool.org>
2021-07-23 17:38:45 -0400

    update nightwatch configuration

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,101 +5,98 @@ module.exports = {
   page_objects_path: './pageobjects',
   globals_path: 'global.js',
 
-  selenium: {
-    start_process: false,
-    host: 'hub-cloud.browserstack.com',
-    port: 443,
-  },
-
-  common_capabilities: {
-    'browserstack.user': process.env.BROWSERSTACK_USER,
-    'browserstack.key': process.env.BROWSERSTACK_KEY,
-  },
-
   test_settings: {
-    qa2_chrome: {
-      launch_url: 'https://qa2.development.tidepool.org',
+    default: {
+      launch_url: 'https://app.tidepool.org',
+    },
+
+    webdriver: {
+      keep_alive: true,
+      timeout_options: {
+        timeout: 60000,
+        retry_sttempts: 3,
+      },
+    },
+
+    browserstack: {
+      selenium: {
+        host: 'hub-cloud.browserstack.com',
+        port: 443,
+      },
+
       desiredCapabilities: {
-        os: 'Windows',
-        osVersion: '10',
+        local: 'false',
+        userName: process.env.BROWSERSTACK_USER,
+        accessKey: process.env.BROWSERSTACK_KEY,
+        'browserstack.networkLogs': 'true',
+        'browserstack.debug': 'true',
+      },
+    },
+
+    'browserstack.qa2chrome': {
+      extends: 'browserstack',
+      desiredCapabilities: {
         browserName: 'chrome',
         browserVersion: 'latest',
+        launch_url: 'https://qa2.development.tidepool.org',
+        os: 'Windows',
+        osVersion: '10',
         resolution: '1366x768',
         build: `QA2_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
-        'browserstack.networkLogs': 'true',
-        'browserstack.debug': 'true',
       },
     },
-    qa1_chrome: {
-      launch_url: 'https://qa1.development.tidepool.org',
+
+    'browserstack.qa1chrome': {
+      extends: 'browserstack',
       desiredCapabilities: {
-        os: 'Windows',
-        osVersion: '10',
         browserName: 'chrome',
         browserVersion: 'latest',
+        launch_url: 'https://qa1.development.tidepool.org',
+        os: 'Windows',
+        osVersion: '10',
         resolution: '1366x768',
         build: `QA1_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
-        'browserstack.networkLogs': 'true',
-        'browserstack.debug': 'true',
       },
     },
-    prd_chrome: {
-      launch_url: 'https://app.tidepool.org',
+
+    'browserstack.intchrome': {
+      extends: 'browserstack',
       desiredCapabilities: {
-        os: 'Windows',
-        osVersion: '10',
         browserName: 'chrome',
         browserVersion: 'latest',
-        resolution: '1366x768',
-        build: `PRODUCTION_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
-        'browserstack.networkLogs': 'true',
-        'browserstack.debug': 'true',
-      },
-    },
-    dev1_chrome: {
-      launch_url: 'https://dev1.dev.tidepool.org',
-      desiredCapabilities: {
+        launch_url: 'https://int-app.tidepool.org',
         os: 'Windows',
         osVersion: '10',
-        browserName: 'chrome',
-        browserVersion: 'latest',
-        resolution: '1366x768',
-        build: `DEV1_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
-        'browserstack.networkLogs': 'true',
-        'browserstack.debug': 'true',
-      },
-    },
-    int_chrome: {
-      launch_url: 'https://int-app.tidepool.org',
-      desiredCapabilities: {
-        os: 'Windows',
-        osVersion: '10',
-        browserName: 'chrome',
-        browserVersion: 'latest',
         resolution: '1366x768',
         build: `INT_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
-        'browserstack.networkLogs': 'true',
-        'browserstack.debug': 'true',
+
+      },
+    },
+
+    'browserstack.prdchrome': {
+      extends: 'browserstack',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        browserVersion: 'latest',
+        launch_url: 'https://app.tidepool.org',
+        os: 'Windows',
+        osVersion: '10',
+        resolution: '1366x768',
+        build: `PRD_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
+      },
+    },
+
+    'browserstack.dev1chrome': {
+      extends: 'browserstack',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        browserVersion: 'latest',
+        launch_url: 'https://dev1.dev.tidepool.org',
+        os: 'Windows',
+        osVersion: '10',
+        resolution: '1366x768',
+        build: `DEV1_CHROME ${dayjs().format('YYYY-MM-DD h:mm:ss A')}`,
       },
     },
   },
-  /*
-    When we're ready to use multi-browser testing we can uncomment these
-    - add names to the test script in both the nightwatch.conf.js and travis.yml
-
-    'qa2_firefox': {
-      'desiredCapabilities': {
-        'browserName': 'firefox'
-      }
-    },
-    'qa2_safari': {
-      "desiredCapabilities": {
-        'browserName': 'safari'
-      }
-    },
-    'qa2_ie': {
-      'desiredCapabilities': {
-        'browserName': "internet explorer"
-      }
-    } */
 };
