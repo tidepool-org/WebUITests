9970b84e0f56330f9d9243b8cf6f8c915b1d19b7
brian-tidepool <brian@tidepool.org>
2024-05-30 22:13:56 -0700

    suppressed notification test
    tests when a user modifies the clinic profile, the supressednotification setting is preserved (true)

---
+++ tests/e2e/suppressedNotificationsFunctional.js
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+require('../../utilities/seleniumKeepAlive');
+
+module.exports = {
+  '@tags': ['notifications'],
+  'Clinician User Logs in with Existing Credentials with notification suppressed'(browser) {
+    const loginPage = browser.page.loginPage();
+    const clinicianUsername = browser.globals.clinicianUsername;
+    const clinicianPassword = browser.globals.clinicianPassword;
+    loginPage.loadPage();
+    loginPage.userLogin(clinicianUsername, clinicianPassword);
+    browser.suppressedNotificationsTrue();
+    browser.suppressedNotificationsTrueCheck();
+  },
+  'Clinic workspace selection'(browser) {
+    const clinicWorkspacePage = browser.page.clinicWorkspacePage();
+    const clinicWorkspace = clinicWorkspacePage.section.clinicWorkspace;
+    clinicWorkspace.waitForElementVisible('@title', browser.globals.elementTimeout);
+    clinicWorkspace.click('@goToWorkspace');
+  },
+  'Clinic workspace settings'(browser) {
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
+    clinicPatientList.waitForElementVisible('@workspaceSettings', browser.globals.elementTimeout);
+    clinicPatientList.click('@workspaceSettings');
+    clinicPatientList.waitForElementVisible('@edit', browser.globals.elementTimeout);
+  },
+  'Clinic workspace settings edit'(browser) {
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
+    clinicPatientList.waitForElementVisible('@edit', browser.globals.elementTimeout);
+    clinicPatientList.click('@edit');
+    clinicPatientList.waitForElementVisible('@city', browser.globals.elementTimeout);
+  },
+  'Clinic workspace settings apply changes and check suppressed notifications status'(browser) {
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
+    clinicPatientList.waitForElementVisible('@city', browser.globals.elementTimeout);
+    clinicPatientList.setValue('@city', 'new');
+    clinicPatientList.click('@clinicProfileSubmit');
+    clinicPatientList.waitForElementNotVisible('@city', browser.globals.elementTimeout);
+    browser.suppressedNotificationsTrueCheck();
+  },
+
+};
