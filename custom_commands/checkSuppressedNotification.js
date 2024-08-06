847ded59b73adb4cacbaf6d4ea48906a622991ea
brian-tidepool <brian@tidepool.org>
2024-08-06 12:26:01 -0700

    add try catch suppressednotifaction

--- custom_commands/checkSuppressedNotification.js
+++ custom_commands/checkSuppressedNotification.js
@@ -36,30 +36,34 @@ module.exports = class checkSuppressedNotification {
      */
       const clinicData = async () => {
         const token = await getToken();
-        const response = await axios.get(`${environment}/v1/clinicians/${clinicianId}/clinics`, {
+        try {
+          const response = await axios.get(`${environment}/v1/clinicians/${clinicianId}/clinics`, {
 
-          params: {
-            limit: '1000',
-            offset: '0',
-          },
-          headers: {
-            authority: this.api.browser_url,
-            accept: '*/*',
-            'accept-language': 'en-US,en;q=0.9',
-            cookie: '_ga=GA1.1.516068539.1692684735; _ga_RWXQ3R57PB=GS1.1.1694192507.4.0.1694192507.0.0.0',
-            referer: this.api.browser_url,
-            'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-            'x-tidepool-session-token': token,
-            'x-tidepool-trace-session': '92df086f-917e-4df5-b32d-cb30d15df795',
-          },
-        });
-        return response.data;
+            params: {
+              limit: '1000',
+              offset: '0',
+            },
+            headers: {
+              authority: this.api.browser_url,
+              accept: '*/*',
+              'accept-language': 'en-US,en;q=0.9',
+              cookie: '_ga=GA1.1.516068539.1692684735; _ga_RWXQ3R57PB=GS1.1.1694192507.4.0.1694192507.0.0.0',
+              referer: this.api.browser_url,
+              'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+              'sec-ch-ua-mobile': '?0',
+              'sec-ch-ua-platform': '"Windows"',
+              'sec-fetch-dest': 'empty',
+              'sec-fetch-mode': 'cors',
+              'sec-fetch-site': 'same-origin',
+              'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+              'x-tidepool-session-token': token,
+              'x-tidepool-trace-session': '92df086f-917e-4df5-b32d-cb30d15df795',
+            },
+          });
+          return response.data;
+        } catch (error) {
+          return console.error(error);
+        }
       };
       /**
        * function calls asynchronous function clinicData and awaits for result
