8e9b69ebb6d657b0f41146e2de0c6bc76750edbe
brian-tidepool <brian@tidepool.org>
2024-08-20 15:01:57 -0700

    update accessCustodialUser tests
    -rename tag qae222 to qae-222
    -rename test to Clinician is able to select workspace
    -add assertion to test

--- tests/e2e/accessCustodialUser.js
+++ tests/e2e/accessCustodialUser.js
@@ -2,7 +2,7 @@
 require('../../utilities/seleniumKeepAlive');
 
 module.exports = {
-  '@tags': ['qae222', 'clinician', 'parallel'],
+  '@tags': ['qae-222', 'clinician', 'parallel'],
   'Clinician User Logs in with Existing Credentials'(browser) {
     const loginPage = browser.page.loginPage();
     const clinicianUsername = browser.globals.clinicianUsername;
@@ -10,11 +10,14 @@ module.exports = {
     loginPage.loadPage();
     loginPage.userLogin(clinicianUsername, clinicianPassword);
   },
-  'Clinic workspace selection'(browser) {
+  'Clinician is able to select workspace'(browser) {
     const clinicWorkspacePage = browser.page.clinicWorkspacePage();
     const clinicWorkspace = clinicWorkspacePage.section.clinicWorkspace;
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
     clinicWorkspace.waitForElementVisible('@title', browser.globals.elementTimeout);
     clinicWorkspace.click('@goToWorkspace');
+    clinicPatientList.expect.element('@showAll').to.be.present.before(browser.globals.elementTimeout);
   },
   'Clinic workspace search custodial user and click patient name'(browser) {
     const clinicPatientListPage = browser.page.clinicPatientListPage();
