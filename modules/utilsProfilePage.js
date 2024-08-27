4777496cdd7f2a17a5630da977ee007f868a4e70
brian-tidepool <brian@tidepool.org>
2024-08-26 21:25:52 -0700

    add tests for export user profile as a file

--- modules/utilsProfilePage.js
+++ modules/utilsProfilePage.js
@@ -1,17 +1,15 @@
 /* eslint-disable linebreak-style */
+const moment = require('moment');
 
 /**
 
- * Checks if a file exists using the browser instance.
- * If the file exists, it asserts that it is true.
- * If the file does not exist, it quits the browser and asserts that it is false.
+ * Reset target range when units selected are mg/dl
  *
  * @param {object} browser - The browser instance.
- * @param {string} fileName - The name of the file to check.
- * @param {number} attemptsCheckFileExists - The number of attempts to check if the file exists.
- * @return {Promise<void>} - A Promise that resolves when the file existence has been checked.
+ * @param {string} userProfile - user profile page object
+ * @return {Promise<void>} - A Promise that resolves after range is reset
+ * and values are verified correct
  */
-
 exports.resetDefaultMGDL = async (browser, userProfile) => {
   userProfile.click('@reset');
   let currentLow;
@@ -29,7 +27,15 @@ exports.resetDefaultMGDL = async (browser, userProfile) => {
   browser.assert.strictEqual(70, currentLow, 'low reset to default 70');
   browser.assert.strictEqual(180, currentHigh, 'high reset to default 180');
 };
+/**
 
+ * Reset target range when units selected are mmol/l
+ *
+ * @param {object} browser - The browser instance.
+ * @param {string} userProfile - user profile page object
+ * @return {Promise<void>} - A Promise that resolves after range is reset
+ * and values are verified correct
+ */
 exports.resetDefaultMMOLL = async (browser, userProfile) => {
   userProfile.click('@reset');
   let currentLow;
@@ -46,4 +52,26 @@ exports.resetDefaultMMOLL = async (browser, userProfile) => {
   });
   browser.assert.strictEqual(3.9, currentLow, 'low reset to default 3.9');
   browser.assert.strictEqual(10.0, currentHigh, 'high reset to default 10.0');
+};
+/**
+
+ * Checks if date values are correct for date range slection
+ *
+ * @param {object} browser - The browser instance.
+ * @param {string} userProfile - user profile page object
+ * @return {Promise<void>} - A Promise that resolves after dates are verified correct
+ */
+exports.checkExportDates = async (browser, userProfile, delta) => {
+  const today = moment();
+  const start = moment().subtract(delta, 'days');
+  const end = today;
+  const startDate = start.format('YYYY-MM-DD');
+  const endDate = end.format('YYYY-MM-DD');
+  let startDateValue;
+  startDateValue = await userProfile.getValue('@startDate');
+  let endDateValue;
+  endDateValue = await userProfile.getValue('@endDate');
+  console.log(`sdv${startDateValue}`);
+  await browser.assert.strictEqual(startDateValue, String(startDate), 'start date set correctly according to offset');
+  await browser.assert.strictEqual(endDateValue, String(endDate), 'end date set correctly according to offset');
 };
---
+++ tests/e2e/exportCustodialUserProfileUnitsMMOLLFileTypeJson.js
@@ -0,0 +1,97 @@
+/* eslint-disable linebreak-style */
+require('../../utilities/seleniumKeepAlive');
+const utilFile = require('../../modules/utilFile');
+const utilsProfilePage = require('../../modules/utilsProfilePage');
+
+module.exports = {
+  '@tags': ['qae-223export', 'clinician', 'parallel'],
+  'Clinician User Logs in with Existing Credentials'(browser) {
+    const loginPage = browser.page.loginPage();
+    const clinicianUsername = browser.globals.clinicianUsername;
+    const clinicianPassword = browser.globals.clinicianPassword;
+    const clinicWorkspacePage = browser.page.clinicWorkspacePage();
+    const clinicWorkspace = clinicWorkspacePage.section.clinicWorkspace;
+    loginPage.loadPage();
+    loginPage.userLogin(clinicianUsername, clinicianPassword);
+    clinicWorkspace.expect.element('@title').to.be.present.before(browser.globals.elementTimeout);
+  },
+  'Clinic workspace selection'(browser) {
+    const clinicWorkspacePage = browser.page.clinicWorkspacePage();
+    const clinicWorkspace = clinicWorkspacePage.section.clinicWorkspace;
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
+    clinicWorkspace.waitForElementVisible('@title', browser.globals.elementTimeout);
+    clinicWorkspace.click('@goToWorkspace');
+    clinicPatientList.expect.element('@showAll').to.be.present.before(browser.globals.elementTimeout);
+  },
+  'Clinic workspace search custodial user and click patient name'(browser) {
+    const clinicPatientListPage = browser.page.clinicPatientListPage();
+    const clinicPatientList = clinicPatientListPage.section.patientList;
+    clinicPatientList.patientFilterSearch('custodial user');
+    clinicPatientList.waitForElementVisible('@row0PatientName', browser.globals.elementTimeout);
+    clinicPatientList.click('@row0PatientName');
+    clinicPatientList.expect.element('@uploadButton').to.be.present.before(browser.globals.elementTimeout);
+    clinicPatientList.expect.element('@resendVerificationEmailButton').to.be.present.before(browser.globals.elementTimeout);
+  },
+  'Click custodial user profile link'(browser) {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.profile;
+    userProfile.waitForElementVisible('@profile', browser.globals.elementTimeout);
+    userProfile.click('@profile');
+    userProfile.expect.element('@edit').to.be.present.before(browser.globals.elementTimeout);
+  },
+  'Click export units and file type'(browser) {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.export;
+    userProfile.waitForElementVisible('@mmoll', browser.globals.elementTimeout);
+    userProfile.click('@mmoll');
+    userProfile.waitForElementVisible('@json', browser.globals.elementTimeout);
+    userProfile.click('@json');
+    userProfile.expect.element('@mmoll').to.be.selected;
+    userProfile.expect.element('@json').to.be.selected;
+  },
+  'Click export in custodial user profile page | date range: all data': async (browser) => {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.export;
+    userProfile.click('@allData');
+    userProfile.expect.element('@startDate').to.have.css('background-color').which.equals('rgba(204, 204, 204, 1)');
+    userProfile.click('@export');
+    const fileName = 'TidepoolExport.json';
+    const attemptsCheckFileExists = 10;
+    await utilFile.checkFileExistence(browser, fileName, attemptsCheckFileExists);
+  },
+  'Click export in custodial user profile page | date range: last 90 days': async (browser) => {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.export;
+    userProfile.click('@last90Days');
+    userProfile.click('@export');
+    const fileName = 'TidepoolExport.json';
+    const attemptsCheckFileExists = 10;
+    await utilFile.checkFileExistence(browser, fileName, attemptsCheckFileExists);
+    const daysDelta = 90;
+    await utilsProfilePage.checkExportDates(browser, userProfile, daysDelta);
+  },
+  'Click export in custodial user profile page | date range: last 30 days': async (browser) => {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.export;
+    userProfile.click('@last30Days');
+    userProfile.click('@export');
+    const fileName = 'TidepoolExport.json';
+    const attemptsCheckFileExists = 10;
+    await utilFile.checkFileExistence(browser, fileName, attemptsCheckFileExists);
+    const daysDelta = 30;
+    await utilsProfilePage.checkExportDates(browser, userProfile, daysDelta);
+  },
+  'Click export in custodial user profile page | date range: last 14 days': async (browser) => {
+    const userProfilePage = browser.page.userProfilePage();
+    const userProfile = userProfilePage.section.export;
+    userProfile.click('@last14Days');
+    userProfile.click('@export');
+    const fileName = 'TidepoolExport.json';
+    const attemptsCheckFileExists = 10;
+    await utilFile.checkFileExistence(browser, fileName, attemptsCheckFileExists);
+    const daysDelta = 14;
+    await utilsProfilePage.checkExportDates(browser, userProfile, daysDelta);
+  },
+
+};
