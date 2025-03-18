7aa4e78803e72eb4dd91c6859259381c71663159
Vojtech Cerveny <cervik49@gmail.com>
2025-03-18 17:28:40 +0100

    chore: Move Playwright PoM into /pageobject/pages to separe pages from sections

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,12 +1,14 @@
 // @ts-check
-import { test, expect } from "@playwright/test";
-import LoginPage from "../../pageobjects/login.page";
+import { expect, test } from "@playwright/test";
+import LoginPage from "../../pageobjects/pages/login.page";
+import WorkspacesPage from "../../pageobjects/pages/workspaces/page";
 import env from "../../utilities/env";
-import WorkspacesPage from "../../pageobjects/workspaces/page";
 
 //Possible testcases: https://tidepool.atlassian.net/jira/software/c/projects/WEB/issues/?jql=project%20%3D%20%22WEB%22%20AND%20type%20%3D%20Test%20AND%20textfields%20~%20%22login%22%20ORDER%20BY%20created%20DESC
 test.describe("Login into application", () => {
-  test("should work with valid credentials for clinician with multiple clinics", async ({ page }) => {
+  test("should work with valid credentials for clinician with multiple clinics", async ({
+    page,
+  }) => {
     const loginPage = new LoginPage(page);
 
     await test.step("When user is logged into application", async () => {
@@ -17,7 +19,7 @@ test.describe("Login into application", () => {
     await test.step("Then the user is redirected to workspaces page", async () => {
       const workspacesPage = new WorkspacesPage(page);
       await page.waitForURL(workspacesPage.url);
-  
+
       await expect(workspacesPage.header).toBeVisible();
     });
   });
