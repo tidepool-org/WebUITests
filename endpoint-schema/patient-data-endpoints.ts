41135457793de0b7dd48de45efb1f16d4dd3a69e
Ginny Yadav <ginny@tidepool.org>
2025-08-11 14:03:58 -0400

    Linting fixes
    Linting, Trailing commas

--- endpoint-schema/patient-data-endpoints.ts
+++ endpoint-schema/patient-data-endpoints.ts
@@ -4,51 +4,51 @@ import { EndpointSchema } from './profile-endpoints';
  * Schema for patient data GET endpoint
  */
 export const getPatientDataSchema: EndpointSchema = {
-  url: /\/v1\/patients\/[^\/]+\/data$/,
+  url: /\/v1\/patients\/[^/]+\/data$/,
   method: 'GET',
   expectedStatus: 200,
   responseSchema: {
     data: 'object',
     meta: {
       count: 'number',
-      size: 'number'
-    }
-  }
+      size: 'number',
+    },
+  },
 };
 
 /**
  * Schema for uploading patient data
  */
 export const uploadPatientDataSchema: EndpointSchema = {
-  url: /\/v1\/patients\/[^\/]+\/data$/,
+  url: /\/v1\/patients\/[^/]+\/data$/,
   method: 'POST',
   expectedStatus: 201,
   requestSchema: {
     data: 'object',
     deviceId: 'string',
-    uploadId: 'string'
+    uploadId: 'string',
   },
   responseSchema: {
     id: 'string',
-    success: 'boolean'
-  }
+    success: 'boolean',
+  },
 };
 
 /**
  * Schema for getting patient settings
  */
 export const getPatientSettingsSchema: EndpointSchema = {
-  url: /\/v1\/patients\/[^\/]+\/settings$/,
+  url: /\/v1\/patients\/[^/]+\/settings$/,
   method: 'GET',
   expectedStatus: 200,
   responseSchema: {
     bgTarget: {
       low: 'number',
-      high: 'number'
+      high: 'number',
     },
     units: {
-      bg: 'string'
+      bg: 'string',
     },
-    siteChangeSource: 'string'
-  }
+    siteChangeSource: 'string',
+  },
 };
