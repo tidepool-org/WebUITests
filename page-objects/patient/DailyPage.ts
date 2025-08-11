c9f946f60646edde0ef52225222ebfec71931d6e
Ginny Yadav <ginny@tidepool.org>
2025-08-11 14:05:41 -0400

    Rename NavigationSubMenu to PatientNav in DailyPage
    Updated imports and references in PatientDataDailyPage to use PatientNav instead of NavigationSubMenu for consistency with the renamed class.

--- page-objects/patient/DailyPage.ts
+++ page-objects/patient/DailyPage.ts
@@ -1,6 +1,6 @@
 import { Page } from '@playwright/test';
 import DailyChartSection from '@components/daily-chart.js';
-import NavigationSubMenu from '@pom/patient/PatientNavigation.js';
+import PatientNav from '@pom/patient/PatientNavigation.js';
 import NavigationSection from '@components/navigation.section.js';
 
 export default class PatientDataDailyPage {
@@ -8,14 +8,14 @@ export default class PatientDataDailyPage {
 
   navigationBar: NavigationSection;
 
-  navigationSubMenu: NavigationSubMenu;
+  navigationSubMenu: PatientNav;
 
   dailyChart: DailyChartSection;
 
   constructor(page: Page) {
     this.page = page;
     this.navigationBar = new NavigationSection(page);
-    this.navigationSubMenu = new NavigationSubMenu(page);
+    this.navigationSubMenu = new PatientNav(page);
 
     this.dailyChart = new DailyChartSection(page);
   }
